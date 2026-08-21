e4b1432a7a8b1cae6d6f9a8adb9b219c3bc9be31
henrydale2813-ui <henrydale2813@student.lvusd.org>
2026-08-21 15:54:42 -0700

    Update Supabase URL to remove endpoint path

--- js/supabase.js
+++ js/supabase.js
@@ -1,4 +1,4 @@
-const SUPABASE_URL = "https://mvimkvyyycfsyvwsqrvr.supabase.co/rest/v1/";
+const SUPABASE_URL = "https://mvimkvyyycfsyvwsqrvr.supabase.co";
 const SUPABASE_KEY = "sb_publishable_IXxv555lJWj6CnGRITlDiw_U1K06ahG";
 
 const supabase = window.supabase.createClient(
